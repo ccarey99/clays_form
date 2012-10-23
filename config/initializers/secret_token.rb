ea8e2f182c34988f1fe1f7341fdc1dc316349740
Chris Shifflett <cshiffle@umw.edu>
2012-10-23 10:10:50 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Claysform::Application.config.secret_token = 'be1969a25c7e77e439cdff1e57f290fee8d462bb7f11fc82ac2756f5846d29d44e1254d1dfaafd32a8d039a90fa8767a9bfbf540e31aa69c5eb10dacc9fae95d'
